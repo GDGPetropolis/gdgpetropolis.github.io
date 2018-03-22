92979fefc56b33b903f901282f51b3a00fc83e5a
Luiz Fernando <lfrprazeres@gmail.com>
2018-03-22 13:13:58 -0300

    v1.2 att 21/03/2018

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-$(function (){
+var btn = document.getElementById('a2');
 
-});
+btn.addEventListener("click", function() {
+  var i = 10;
+  var int = setInterval(function() {
+    window.scrollTo(0, i);
+    i += 10;
+    if (i >= 200) clearInterval(int);
+  }, 20);
+})
