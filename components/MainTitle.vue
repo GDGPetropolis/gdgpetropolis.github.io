d8db866c764cd482a30378c1e492c8110c03b5aa
Johnathan Fercher <johnathanfercher22@gmail.com>
2019-02-03 15:03:00 -0200

    center

--- components/MainTitle.vue
+++ components/MainTitle.vue
@@ -1,11 +1,5 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>{{title}}</h1>
-            </div>
-        </div>
-    </div>
+    <h1>{{title}}</h1>
 </template>
 
 <script>
