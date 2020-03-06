1176d59d0f2ba4a64c8a50c9f3632c5d63a4daf9
Marcos Nunes <marcosrenatovnunes@hotmail.com>
2020-03-06 17:24:39 -0300

    update trailingslash

--- next.config.js
+++ next.config.js
@@ -1,28 +1,30 @@
 /* eslint-disable */
-const withCss = require('@zeit/next-css')
+const withCss = require("@zeit/next-css");
 
 module.exports = withCss({
+  assetPrefix: "./",
+  exportTrailingSlash: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/
-      const origExternals = [...config.externals]
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
       config.externals = [
         (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
           } else {
-            callback()
+            callback();
           }
         },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ]
+        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+      ];
 
       config.module.rules.unshift({
         test: antStyles,
-        use: 'null-loader',
-      })
+        use: "null-loader"
+      });
     }
-    return config
-  },
-})
+    return config;
+  }
+});
